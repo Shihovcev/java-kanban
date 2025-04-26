4ae789f0464d849955abf7aeb35d3114c9724e2a
Shihovcev <Shihovcev.sv@yandex.ru>
2025-04-26 10:48:55 +0300

    Revert "Добавление функциональности по техническому заданию спринта"

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -3,12 +3,8 @@ package manager;
 import tasks.Task;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 public interface HistoryManager {
     void add(Task task);
-
     ArrayList<Task> getHistory();
-
-    void remove(Integer id);
 }
